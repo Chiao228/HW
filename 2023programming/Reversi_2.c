d6542541998ecb5495357ea87f94dbcc3874ebdb
Chiao228 <148021864+Chiao228@users.noreply.github.com>
2023-10-31 23:58:38 +0800

    Update Reversi_2.c

--- 2023programming/Reversi_2.c
+++ 2023programming/Reversi_2.c
@@ -1,66 +1,9 @@
-#include <stdio.h>
-#define Spacing 0
-#define Black 1
-#define White 2
+# include <stdio.h>
+# define Spacing 0
+# define Black 1
+# define White 2
 
-int next[8][8];
-int newStep(int chess[8][8], int x, int y, int dx, int dy){
-    int find_color, color = chess[x][y];
-
-    if(color == Black){
-        find_color = White;
-    }
-    else{
-        find_color = Black;
-    }
-    for(int dx = -1 ; dx <= 1 ; dx++){
-         for(int dy = -1 ; dy <=1 ; dy++){
-             if(dx == 0 && dy == 0){
-                    continue;
-              }
-
-              int newX = x+dx;
-              int newY = y+dy;
-
-              while(newX >= 0 && newX < 8 && newY >= 0 && newY < 8 && chess[newX][newY] == find_color){
-                     newX += dx;
-                     newY += dy;
-                     if(chess[newX][newY] == 0){
-                         next[newX][newY] = color;
-                     }
-               }
-           }
-       }
-}
-
-
-
-
-void check_new_step_by_color(int chess[8][8], int next[8][8], int color){
-
-}
-
-void check_new_step(int chess[8][8], int next[8][8], int x, int y){
-
-}
-
-int new_step(int chess[8][8], int next[8][8], int x, int y, int dx, int dy){
-
-    return 0;
-    return 1;
-}
-
-void print_chessboard(int chess[8][8]){
-
-    for(int i = 0; i < 8; i++){
-        for(int j = 0; j < 8; j++){
-            printf("%d ", chess[i][j]);
-        }
-        printf("\n");
-    }
-}
-int main(){
-    int chess[8][8] = {
+int next[8][8]={
           {0,0,0,0,0,0,0,0},
           {0,0,0,0,0,0,0,0},
           {0,0,0,0,0,0,0,0},
@@ -70,9 +13,99 @@ int main(){
           {0,0,0,0,0,0,0,0},
           {0,0,0,0,0,0,0,0}
     };
-    int x, y, color;
-    print_chessboard(chess);
-    scanf("%d %d",&x ,&y);
-    return 0;
+int chess[8][8]={
+          {0,0,0,0,0,0,0,0},
+          {0,0,0,0,0,0,0,0},
+          {0,0,0,0,0,0,0,0},
+          {0,0,0,1,2,0,0,0},
+          {0,0,0,2,1,0,0,0},
+          {0,0,0,0,0,0,0,0},
+          {0,0,0,0,0,0,0,0},
+          {0,0,0,0,0,0,0,0}
+    };
+
+int new_step(int x, int y){
+    int find_color, color , new_x , new_y;
+    for(int i =3 ; i<5 ; i++){
+        for(int j=3 ; j<5 ; j++){
+             color = chess[i][j];
+             if(color == Black){
+                find_color = White;
+             }
+             else{
+                find_color = Black;
+             }
+             for(int dx = -1 ; dx <= 1 ; dx++){
+                for(int dy = -1 ; dy <=1 ; dy++){
+                    if(dx == 0 && dy == 0){
+                        continue;
+                    }
+                    new_x = i+dx;
+                    new_y = j+dy;
+                    while(new_x >= 0 && new_x < 8 && new_y >= 0 && new_y < 8 && chess[new_x][new_y] == find_color){
+                        new_x += dx;
+                        new_y += dy;
+                         if(chess[new_x][new_y] == 0){
+                             next[new_x][new_y] = color;
+                             if(i == x && j == y){
+                                printf("(%d,%d)",new_x ,new_y);
+                             }
+                         }
+                    }
+                }
+             }
+        }
+    }
 }
 
+void print_can_move(){
+    for(int i = 0; i < 8; i++){
+        for(int j = 0; j < 8; j++){
+            printf("%d ", next[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void printf_chess_Black(int x , int y){
+    for(int i = 0; i < 8; i++){
+        for(int j = 0; j < 8; j++){
+            if(chess[i][j] != 0){
+                continue;
+            }
+            else{
+                if(next[i][j] == Black){
+                    printf("(%d,%d)\t",i ,j);
+                }
+            }
+        }
+    }
+}
+
+void printf_chess_White(int x , int y){
+    for(int i = 0; i < 8; i++){
+        for(int j = 0; j < 8; j++){
+            if(chess[i][j] != 0){
+                continue;
+            }
+            else{
+                if(next[i][j] == White){
+                    printf("(%d,%d)\t",i ,j);
+                }
+            }
+        }
+    }
+}
+
+int main(){
+    int x, y;
+    scanf("%d %d",&x , &y);
+    new_step(x, y);
+    printf("\n");
+    print_can_move();
+    printf("白子可下位置:\n");
+    printf_chess_White(x,y);
+    printf("\n");
+    printf("黑子可下位置:\n");
+    printf_chess_Black(x,y);
+}
