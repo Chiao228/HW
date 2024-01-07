6618a5f9b2efe8eb6afc9458bc4f34415bfbfbff
Chiao228 <148021864+Chiao228@users.noreply.github.com>
2024-01-07 23:52:45 +0800

    Add files via upload

---
+++ 2023programming/maze_v1.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+
+#define MAX_N 100
+int stack_i[100],stack_j[100];
+int top=0;
+typedef struct{
+    int i;
+    int j;
+}coordinate;
+
+void push(int i,int j){
+    stack_i[top]=i;
+    stack_j[top]=j;
+    top++;
+}
+coordinate pop(){
+    coordinate temp;
+    temp.i=stack_i[top-1];
+    temp.j=stack_j[top-1];
+    top--;
+    return temp;
+}
+int is_empty(int i,int j){
+    if(top==0)
+        return 0;
+    return 1;
+}
+
+int main() {
+    char maze[MAX_N][MAX_N] = {
+        { '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'},
+        { '0', '0', '0', '1', '1', '0', '1', '1', '0', '1'},
+        { '1', '0', '1', '1', '0','0', '0', '0', '0', '1' },
+        { '1', '0', '0', '0', '0', '1', '0', '1', '0', '1'},
+        { '1', '1', '1', '0', '1', '1', '0', '1', '1', '1'},
+        { '1', '0', '1', '1', '1', '0', '0', '1', '0', '1'},
+        { '1', '0', '0', '0','0', '0', '1', '1', '0', '0'},
+        { '1', '0', '1', '1', '1', '1', '1', '0', '0', '1'},
+        { '1', '0', '0', '0', '0', '0', '0', '0', '1', '1'},
+        { '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'}
+    };
+    int start_i,start_j,end_i,end_j;
+    printf("Enterance:");
+    scanf("%d %d",&start_i,&start_j);
+
+    printf("Exit:");
+    scanf("%d %d",&end_i,&end_j);
+    push(start_i,start_j);
+    int d[][2]={{-1,0},{1,0},{0,-1},{0,1}};
+    int i,j;
+    int visited[MAX_N][MAX_N]={{0}};
+    while(is_empty(i,j)){
+        coordinate current=pop();
+        maze[current.i][current.j]='x';
+        visited[current.i][current.j]= 2;
+        if(current.i==end_i && current.j==end_j){
+            for(int y=0;y<MAX_N;y++){
+                for(int x=0;x<MAX_N;x++){
+                    printf("%c",maze[y][x]);
+                }
+                printf("\n");
+            }
+            break;
+        }
+        else{
+            int flag=0;
+            for(int go=0;go<4;go++){
+                if(current.i+d[go][0]>=0 && current.i+d[go][0]<MAX_N && current.j+d[go][1]>=0 && current.j+d[go][1]<MAX_N && maze[current.i+d[go][0]][current.j+d[go][1]]=='0'&& visited[current.i+d[go][0]][current.j+d[go][1]]==0){
+                    push(current.i+d[go][0],current.j+d[go][1]);
+                    flag=1;
+                }
+            }
+            if(flag==0)
+            {
+                maze[current.i][current.j]='0';
+                visited[current.i][current.j]=1;
+            }
+        }
+    }
+}
